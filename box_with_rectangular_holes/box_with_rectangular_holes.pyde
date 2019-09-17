549bafd5e9e1e04cafbe7c5b8aa9fb9a34c5b137
Alexandre B A Villares <abav@lugaralgum.com>
2019-09-16 23:26:21 -0300

    refactor
    e -> thick
    sides -> edges
    EN comments
    more comments
    EN variable names

--- box_with_rectangular_holes/box_with_rectangular_holes.pyde
+++ box_with_rectangular_holes/box_with_rectangular_holes.pyde
@@ -1,7 +1,12 @@
-# Alexandre B A Villares - https://abav.lugaralgum.com/sketch-a-day
+"""
+Alexandre B A Villares - https://abav.lugaralgum.com/sketch-a-day
 
-from frame_box import frame_box, unfolded_box
+Box with rectangular holes
+"""
 
+from frame_box import frame_box, unfolded_frame_box
+
+w, h, d, thick = 250, 150, 100, 30
 modes = [-1, 0, 1] # click mouse to switch modes
 
 def setup():
@@ -16,27 +21,15 @@ def draw():
         pushMatrix()
         translate(0, 0, 200)
         rotateX(HALF_PI / 2)
-        frame_box(250, 150, 100, 30)
+        frame_box(w, h, d, thick)
         popMatrix()
     if modes[0] <= 0:
-        unfolded_box(250, 150, 100, 30)
+        unfolded_frame_box(w, h, d, thick)
 
 def mousePressed():
     modes[:] = modes[1:] + [modes[0]]
     
 def keyPressed():
-    saveFrame("a###.png")
+    if key == "s":
+        saveFrame("a###.png")
     
-# para gerar o markdown que vai na página do sketch-a-day    
-def settings():
-    from os import path
-    global SKETCH_NAME
-    SKETCH_NAME = path.basename(sketchPath())
-    OUTPUT = ".gif"
-    println(
-        """
-![{0}]({2}/{0}/{0}{1})
-
-[{0}](https://github.com/villares/sketch-a-day/tree/master/{2}/{0}) [[Py.Processing](https://villares.github.io/como-instalar-o-processing-modo-python/index-EN)]
-""".format(SKETCH_NAME, OUTPUT, year())
-    )
--- box_with_rectangular_holes/frame_box.py
+++ box_with_rectangular_holes/frame_box.py
@@ -1,6 +1,6 @@
-CORTE, VINCO = color(255, 0, 0), color(0, 0, 255)
+CUT_STROKE, FOLD_STROKE = color(255, 0, 0), color(0, 0, 255)
 
-def face(x, y, w, h, e):
+def face(x, y, w, h, thick):
     mw, mh = w / 2., h / 2.
     pushMatrix()
     translate(x, y)
@@ -9,73 +9,73 @@ def face(x, y, w, h, e):
     vertex(+mw, -mh)
     vertex(+mw, +mh)
     vertex(-mw, +mh)
-    if e > 0 and mw - e > 0 and mh - e > 0:
-        mw -= e
-        mh -= e
-        beginContour()
+    if thick > 0 and mw - thick > 0 and mh - thick > 0:
+        mw -= thick
+        mh -= thick
+        beginContour()  # counterclockwise hole
         vertex(-mw, -mh)
-        vertex(-mw, +mh)        
+        vertex(-mw, +mh)
         vertex(+mw, +mh)
         vertex(+mw, -mh)
         endContour()
     endShape(CLOSE)
     popMatrix()
-    
-def frame_box(w, h, d, e=0):
-     mw, mh, md = w/2., h/2., d/2.
-     translate(0, 0, -md) # base
-     face(0, 0, w, h, e)
-     translate(0, 0, d) # topo
-     face(0, 0, w, h, e)
-     translate(0, 0, -md) # volta
-     rotateY(HALF_PI)
-     translate(0, 0, -mw) # lateral e
-     face(0, 0, d, h, e)
-     translate(0, 0, w) # lateral d
-     face(0, 0, d, h, e)
-     translate(0, 0, -mw) # volta    
-     rotateY(-HALF_PI)  # volta
-     rotateX(HALF_PI)
-     translate(0, 0, -mh) # lateral e
-     face(0, 0, w, d, e)
-     translate(0, 0, h) # lateral d
-     face(0, 0, w, d, e)
-     translate(0, 0, -mw) # volta         
-     rotateX(-HALF_PI)
 
-def unfolded_box(w, h, d, e=0, draw_main=True):
+def frame_box(w, h, d, thick=0):
     mw, mh, md = w / 2., h / 2., d / 2.
-    face_unfold(0, -h - md, w, d, "aaan", e, draw_main)
-    face_unfold(0, -mh, w, h, "vvvv", e, draw_main)
-    face_unfold(0, -mh + mh + md, w, d, "cncv", e, draw_main)
-    face_unfold(0, +mh + d, w, h, "cncc", e, draw_main)
-    face_unfold(-mw - md, -mh, d, h, "acna", e, draw_main)
-    face_unfold(mw + md, -mh, d, h, "ncaa", e, draw_main)
+    translate(0, 0, -md)  # base
+    face(0, 0, w, h, thick)
+    translate(0, 0, d)  # top
+    face(0, 0, w, h, thick)
+    translate(0, 0, -md)  # back to 0
+    rotateY(HALF_PI)
+    translate(0, 0, -mw)  # left side
+    face(0, 0, d, h, thick)
+    translate(0, 0, w)  # right side
+    face(0, 0, d, h, thick)
+    translate(0, 0, -mw)  # back to middle
+    rotateY(-HALF_PI)  # back to 0 rotation
+    rotateX(HALF_PI)
+    translate(0, 0, -mh)  # lateral e
+    face(0, 0, w, d, thick)
+    translate(0, 0, h)  # lateral d
+    face(0, 0, w, d, thick)
+    translate(0, 0, -mw)  # reset translate
+    rotateX(-HALF_PI)  # reset rotate
 
-def face_unfold(x, y, w, h, sides, e=0, draw_main=True):
-    l0, l1, l2, l3 = sides
+def unfolded_frame_box(w, h, d, thick=0, draw_main=True):
+    mw, mh, md = w / 2., h / 2., d / 2.
+    face_unfold(0, -h - md, w, d, "aaan", thick, draw_main)
+    face_unfold(0, -mh, w, h, "vvvv", thick, draw_main)
+    face_unfold(0, -mh + mh + md, w, d, "cncv", thick, draw_main)
+    face_unfold(0, +mh + d, w, h, "cncc", thick, draw_main)
+    face_unfold(-mw - md, -mh, d, h, "acna", thick, draw_main)
+    face_unfold(mw + md, -mh, d, h, "ncaa", thick, draw_main)
+
+def face_unfold(x, y, w, h, edge_types, thick=0, draw_main=True):
+    e0, e1, e2, e3 = edge_types
     mw, mh = w / 2., h / 2.
     pushMatrix()
     translate(x, y)
     if draw_main:
-        edge(-mw, +mh, -mw, -mh, l0)
-        edge(-mw, -mh, +mw, -mh, l1)
-        edge(+mw, -mh, +mw, +mh, l2)
-        edge(+mw, +mh, -mw, +mh, l3)
-    if e > 0 and mw - e > 0 and mh - e > 0:
-        face_unfold(0, 0, w - e * 2, h - e * 2, "cccc")
+        edge(-mw, +mh, -mw, -mh, e0)
+        edge(-mw, -mh, +mw, -mh, e1)
+        edge(+mw, -mh, +mw, +mh, e2)
+        edge(+mw, +mh, -mw, +mh, e3)
+    if thick > 0 and mw - thick > 0 and mh - thick > 0:
+        face_unfold(0, 0, w - thick * 2, h - thick * 2, "cccc")
     popMatrix()
 
 def edge(x0, y0, x1, y1, edge_type):
-    if edge_type == "n":
+    if edge_type == "n":  # no edge is drawn
         return
-    elif edge_type == "c":
-        stroke(CORTE)
+    elif edge_type == "c":  # cut stroke selected
+        stroke(CUT_STROKE)
     else:
-        stroke(VINCO)
-    line(x0, y0, x1, y1)
-    if edge_type == "a":
-        stroke(CORTE)
+        stroke(FOLD_STROKE)  # fold stroke selected for "v" and "a"
+    line(x0, y0, x1, y1)    # line drawn here
+    if edge_type == "a":    # tab (note a fold-stroke line was already drawn)
+        stroke(CUT_STROKE)
         noFill()
         glue_tab((x0, y0), (x1, y1), 10)
 
